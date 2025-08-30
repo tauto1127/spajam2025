74537af3697fa8a5ff4072dfa376d90c231443e5
takuto1127 <tiharu717@gmail.com>
2025-08-30 21:50:43 +0900

    Initial Commit

---
+++ spajam2025 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spajam2025 Watch App
+//
+//  Created by 梶村拓斗 on 2025/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spajam2025 Watch App/spajam2025App.swift
@@ -0,0 +1,17 @@
+//
+//  spajam2025App.swift
+//  spajam2025 Watch App
+//
+//  Created by 梶村拓斗 on 2025/08/30.
+//
+
+import SwiftUI
+
+@main
+struct spajam2025_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
